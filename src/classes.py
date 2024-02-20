1be1c6d512f1c2fd6f013c6ec6fc3acb60989b23
Petrow_Alexey <l.petrow@mail.ru>
2024-02-20 20:54:05 +0300

    task1,2

---
+++ src/classes.py
@@ -0,0 +1,31 @@
+class Category:
+    """Класс категории"""
+    name: str
+    description: str
+    goods: str
+
+    total_numbers_of_category = 0
+    unique_goods = 0
+
+    def __init__(self, name, description, goods):
+        self.name = name
+        self.description = description
+        self.goods = goods
+
+        Category.total_numbers_of_category += 1
+        Category.unique_goods += 1
+
+
+class Product:
+    """Классы продукта"""
+    name: str
+    description: str
+    price: float
+    quantity_in_stock: int
+
+    def __init__(self, name, description, price, quantity_in_stock):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity_in_stock = quantity_in_stock
+
