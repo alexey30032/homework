61a629ede066a98f7bdd5c4c915faaea649ff6e4
Petrow_Alexey <l.petrow@mail.ru>
2024-02-22 16:06:30 +0300

    fix3

--- src/classes.py
+++ src/classes.py
@@ -5,7 +5,7 @@ class Category:
     goods: str
 
     total_numbers_of_category = 0
-    unique_goods = 0
+    unique_goods = len(goods)
 
     def __init__(self, name, description, goods):
         self.name = name
@@ -13,7 +13,6 @@ class Category:
         self.goods = goods
 
         Category.total_numbers_of_category += 1
-        Category.unique_goods = len(goods)
 
 
 class Product:
