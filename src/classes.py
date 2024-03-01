f30364e998644cbb843b811e51a735f2e9f33257
Petrow_Alexey <l.petrow@mail.ru>
2024-03-01 13:29:20 +0300

    fix

--- src/classes.py
+++ src/classes.py
@@ -15,7 +15,7 @@ class Category:
         Category.total_numbers_of_category += 1
         Category.unique_goods += len(goods)
 
-    def goods(self, product):
+    def goods_list(self, product):
         """Получает обьект Product на вход и добавляет его в список"""
         self.__goods.append({"name": product[0],
                              "description": product[1],
@@ -28,42 +28,47 @@ class Category:
         list_goods = []
         for item in self.__goods:
             list_goods.append(f'{item["name"]}, {item["price"]} руб. Остаток: {item["quantity_in_stock"]} шт.\n')
-            return "".join(list_goods)
+        return "".join(list_goods)
 
 
 class Product:
     """Класс продукта"""
-    name: str
-    description: str
-    price: float
-    quantity_in_stock: int
-
-    def __init__(self, name, description, price, quantity_in_stock):
-        self.name = name
-        self.description = description
-        self.price = price
-        self.quantity_in_stock = quantity_in_stock
 
     @property
     def price(self):
         return self.price
 
+    name: str
+    description: str
+    price: float
+    quantity_in_stock: int
+
+    def __init__(self, name, description, amount, quantity_in_stock):
+        self.name = name
+        self.description = description
+        self.amount = amount
+        self.quantity_in_stock = quantity_in_stock
+
+    @property
+    def get_price(self):
+        return self.amount
+
     @price.setter
     def price(self, new_price):
-        if new_price <= 0 or new_price == self.price:
+        if new_price <= 0 or new_price == self.get_price:
             print(f'Некорректное значение цены')
-        elif new_price < self.price:
+        elif new_price < self.get_price:
             while True:
                 user_answ = input("Новая цена ниже чем старая, вы уверены что хотите изменить цену? "
                                   "(y/n): ").lower()
                 if user_answ == 'y':
-                    self.price = new_price
+                    self.amount = new_price
                     break
                 else:
-                    self.price = self.price
+                    self.amount = self.amount
                     break
         else:
-            self.price = new_price
+            self.amount = new_price
 
     @classmethod
     def create_product(cls, product):
